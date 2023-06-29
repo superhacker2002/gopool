d1ce2bc98fb1ec5135ef59dcb47ab7bac9517920
rubiezzy <rubiezzy@student.21-school.ru>
2023-06-30 00:34:39 +0300

    Add files for 01

--- day02/src/wc/options/options.go
+++ day02/src/wc/options/options.go
@@ -26,20 +26,10 @@ func New() (Options, error) {
 
 	flag.Parse()
 
-	err := oneFlagIsSet([]bool{*lines, *characters, *words})
-
-	if err == ErrNoOptions {
-		*words = true
-	}
-
+	files := os.Args[2:]
+	err := validate([]bool{*lines, *characters, *words}, files)
 	if err != nil {
 		return Options{}, err
-	}
-
-	files := os.Args[2:]
-
-	if len(files) == 0 {
-		return Options{}, ErrNoFiles
 	}
 
 	return Options{
@@ -50,7 +40,7 @@ func New() (Options, error) {
 	}, nil
 }
 
-func oneFlagIsSet(flags []bool) error {
+func validate(flags []bool, files []string) error {
 	setFlags := 0
 	for _, f := range flags {
 		if f {
@@ -60,8 +50,13 @@ func oneFlagIsSet(flags []bool) error {
 	if setFlags > 1 {
 		return ErrMultipleOptions
 	}
+
 	if setFlags == 0 {
-		return ErrNoOptions
+		flags[2] = true
+	}
+
+	if len(files) == 0 {
+		return ErrNoFiles
 	}
 
 	return nil
